1d16e84303f44af98ff0c5559e494f7048b72e71
BerialJ <42025656+BerialJ@users.noreply.github.com>
2023-01-17 16:00:27 +0800

    Create wallpaper_change_cron.py

---
+++ wallpaper_change_cron.py
@@ -0,0 +1,17 @@
+table = {
+    '09:00': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\Konachan.com - 217312 animal brown_eyes '
+            'brown_hair cat drink original polychromatic shion_(mirudakemann) tail.jpg',
+    '11:48': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\0179.jpg',
+    '12:00': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\1536543043040.jpg',
+    '13:30': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\Konachan.com - 266830 black_hair jun_('
+             '5455454541) men_in_black night original parody rain short_hair sunglasses water.jpg',
+    '15:30': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\Konachan.com - 217312 animal '
+             'brown_eyes brown_hair cat drink original polychromatic shion_(mirudakemann) tail.jpg',
+    '16:00': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\0.mp4_20161117_222435.817.jpg',
+    'sunset_time': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\1534677221882.jpg',
+    '18:30': 'C:\\Users\\admin\\Desktop\\tools\\wallpaper_changer\\wallpapers\\1536435442196.jpg',
+    # '':''wallpapers\\\\',
+    # '':''wallpapers\\\\',
+    # '':''wallpapers\\\\',
+}
+check_sunset_time = '15:00'
